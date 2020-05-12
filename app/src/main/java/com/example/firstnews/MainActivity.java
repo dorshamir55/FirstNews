0152bf5f6b284fdfd4349f0d383ca3df2e80cac3
Dor <dorshamir4@gmail.com>
2020-05-12 15:05:06 +0300

    no xml

--- app/src/main/java/com/example/firstnews/MainActivity.java
+++ app/src/main/java/com/example/firstnews/MainActivity.java
@@ -1,25 +1,74 @@
 package com.example.firstnews;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class MainActivity extends AppCompatActivity {
+
+    final int LOCATION_PERMISSION_REQUEST = 1;
+    TextView textView;
+    FusedLocationProviderClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(Build.VERSION.SDK_INT>=23){
+            int hasLocationPermission = checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION);
+            if(hasLocationPermission != getPackageManager().PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            }
+            else
+                startLocation();
+        }
+        else
+            startLocation();
+
+
         ViewPager weatherPager = findViewById(R.id.weather_pager);
         DayAdapter adapter = new DayAdapter(getSupportFragmentManager());
         weatherPager.setAdapter(adapter);
 
 
+    }
+
+    public void startLocation(){
+        client = LocationServices.getFusedLocationProviderClient(this);
+        LocationCallback callback = new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                Location lastlocation  = locationResult.getLastLocation();
+                textView.setText(String.valueOf(lastlocation.getLongitude())+" , "+lastlocation.getLatitude());
+
+            }
+        };
     }
 
     private class DayAdapter extends FragmentStatePagerAdapter {
@@ -36,6 +85,35 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             return Day.values().length;
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == LOCATION_PERMISSION_REQUEST){
+            if(grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(R.string.error).setMessage(R.string.permission_msg)
+                        .setPositiveButton(R.string.settings, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        intent.setData(Uri.parse("package:"+getPackageName()));
+                        startActivity(intent);
+
+                    }
+                })
+                        .setNegativeButton(R.string.quit, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        }).setCancelable(false).show();
+            }
+            else
+                startLocation();
         }
     }
 }
