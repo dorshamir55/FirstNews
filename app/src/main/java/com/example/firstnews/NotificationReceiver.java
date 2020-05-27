6fc013f3cd036de0093502125b4f4bd4e1fb3821
Dor <dorshamir4@gmail.com>
2020-05-27 22:40:29 +0300

    All works! problems - turn on gps generated! Finally!!!

--- app/src/main/java/com/example/firstnews/NotificationReceiver.java
+++ app/src/main/java/com/example/firstnews/NotificationReceiver.java
@@ -160,7 +160,7 @@ public class NotificationReceiver extends BroadcastReceiver {
                     @Override
                     public void run() {
                         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
-                        notification = builder.setContentTitle("מזג האוויר ב"+sp.getString("במיקומך", "מיקומך"))
+                        notification = builder.setContentTitle("מזג האוויר ב"+sp.getString("city_weather", "מיקומך"))
                                 .setContentText(sp.getString("description_weather", "שמיים בהירים")+" "+sp.getString("celsius_weather", "26.79"))
                                 .setAutoCancel(true).setSmallIcon(R.drawable.news_icon).build();
                         notification.defaults = Notification.DEFAULT_VIBRATE|Notification.DEFAULT_LIGHTS;
